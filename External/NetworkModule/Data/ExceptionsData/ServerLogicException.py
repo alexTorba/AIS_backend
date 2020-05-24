7959baf328173df7f0191d9492d2b9fd1ba3e33e
AleksandrTorba <o.torba@plarium.com>
2020-05-24 16:29:25 +0300

    refactory project

--- External/NetworkModule/Data/ExceptionsData/ServerLogicException.py
+++
@@ -1,7 +0,0 @@
-class ServerLogicException(Exception):
-    state_code: int
-    message: str
-
-    def __init__(self, state_code: int, message: str):
-        self.state_code = state_code
-        self.message = message
